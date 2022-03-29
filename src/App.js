cb693f2a3d4356b4e4eeb03ef2e45d2b9423ff3c
shivesh-12 <shiveshr9@gmail.com>
2022-03-29 07:23:28 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import { useSelector, useDispatch } from 'react-redux';
+import { authActions } from './store/index';
 
 function App() {
   return (
